1c709b74da9ec1cb80472dc7de2ba4fe20707aad
nomissivraj <simonpeterjarvis@gmail.com>
2019-03-04 19:55:39 +0000

    initial commit - moved toggle display function to general function file

---
+++ scripts/funcs.js
@@ -0,0 +1,10 @@
+function toggleDisplay(el) {
+    el = document.getElementById(el);
+    if (!el) return; 
+    
+    if (el.style.display === 'none') {
+        el.style.display = 'block';
+    } else {
+        el.style.display = 'none';
+    }
+}
