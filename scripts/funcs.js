7663d5afc2ffb7a0f0478b6b02f973ef7f9c2bfb
nomissivraj <simonpeterjarvis@gmail.com>
2019-04-19 16:47:01 +0100

    tabs functionality

--- scripts/funcs.js
+++ scripts/funcs.js
@@ -115,13 +115,13 @@ function initMenu(menuEl, btn, subBtns) {
         //      - Edit menu 
         let undoBtn = document.getElementById('undobtn');
         undoBtn.addEventListener('click', () => {
-            editor.doc.undo();
+            editors[currentEditor].doc.undo();
             toggleDisplay('dropdown__menu--edit');
         });
 
         let redoBtn = document.getElementById('redobtn');
         redoBtn.addEventListener('click', () => {
-            editor.doc.redo();
+            editors[currentEditor].doc.redo();
             toggleDisplay('dropdown__menu--edit');
         });
 
@@ -152,81 +152,179 @@ function initMenu(menuEl, btn, subBtns) {
 
         let enterBtn = document.getElementById('enterbtn');
         enterBtn.addEventListener('click', () => {
-            CodeMirror.commands.newlineAndIndent(editor);
+            CodeMirror.commands.newlineAndIndent(editors[currentEditor]);
             toggleDisplay('dropdown__menu--edit');
         });
 
         let tabBtn = document.getElementById('tabbtn');
         tabBtn.addEventListener('click', () => {
-            CodeMirror.commands.defaultTab(editor);
+            CodeMirror.commands.defaultTab(editors[currentEditor]);
             toggleDisplay('dropdown__menu--edit');
         });
 
         let spaceBtn = document.getElementById('spacebtn');
         spaceBtn.addEventListener('click', () => {
-            let cursorPos = editor.getCursor();
-            editor.replaceRange(" ",{line: cursorPos.line, ch: cursorPos.ch});
+            let cursorPos = editors[currentEditor].getCursor();
+            editors[currentEditor].replaceRange(" ",{line: cursorPos.line, ch: cursorPos.ch});
             toggleDisplay('dropdown__menu--edit');
         });
 
         //Preview Menu
         let previewBtn = document.getElementById('previewbtn');
         previewBtn.addEventListener('click', () => {
-            let path = savesPath.replace(/\\/g, "/");
-            let path2 = path.replace(/\s+/g, '%20');
-            shell.openExternal('file:///'+path2+currentProject+'/index.html');
+            pagePreview();
             toggleDisplay('dropdown__menu--preview');
         })
 
         //Tags Menu
         let heading1 = document.getElementById('heading1btn');
         heading1.addEventListener('click', () => {
-            let cursorPos = editor.getCursor();
-            editor.replaceRange("<h1></h1>",{line: cursorPos.line, ch: cursorPos.ch});
+            let cursorPos = editors[currentEditor].getCursor();
+            editors[currentEditor].replaceRange("<h1></h1>",{line: cursorPos.line, ch: cursorPos.ch});
             toggleDisplay('dropdown__menu--tags');
         });
 
         let heading2 = document.getElementById('heading2btn');
         heading2.addEventListener('click', () => {
-            let cursorPos = editor.getCursor();
-            editor.replaceRange("<h2></h2>",{line: cursorPos.line, ch: cursorPos.ch});
+            let cursorPos = editors[currentEditor].getCursor();
+            editors[currentEditor].replaceRange("<h2></h2>",{line: cursorPos.line, ch: cursorPos.ch});
             toggleDisplay('dropdown__menu--tags');
         });
 
         let heading3 = document.getElementById('heading3btn');
         heading3.addEventListener('click', () => {
-            let cursorPos = editor.getCursor();
-            editor.replaceRange("<h3></h3>",{line: cursorPos.line, ch: cursorPos.ch});
+            let cursorPos = editors[currentEditor].getCursor();
+            editors[currentEditor].replaceRange("<h3></h3>",{line: cursorPos.line, ch: cursorPos.ch});
             toggleDisplay('dropdown__menu--tags');
         });
 
         let heading4 = document.getElementById('heading4btn');
         heading4.addEventListener('click', () => {
-            let cursorPos = editor.getCursor();
-            editor.replaceRange("<h4></h4>",{line: cursorPos.line, ch: cursorPos.ch});
+            let cursorPos = editors[currentEditor].getCursor();
+            editors[currentEditor].replaceRange("<h4></h4>",{line: cursorPos.line, ch: cursorPos.ch});
             toggleDisplay('dropdown__menu--tags');
         });
 
         let heading5 = document.getElementById('heading5btn');
         heading5.addEventListener('click', () => {
-            let cursorPos = editor.getCursor();
-            editor.replaceRange("<h5></h5>",{line: cursorPos.line, ch: cursorPos.ch});
+            let cursorPos = editors[currentEditor].getCursor();
+            editors[currentEditor].replaceRange("<h5></h5>",{line: cursorPos.line, ch: cursorPos.ch});
             toggleDisplay('dropdown__menu--tags');
         });
 
         let heading6 = document.getElementById('heading6btn');
         heading6.addEventListener('click', () => {
-            let cursorPos = editor.getCursor();
-            editor.replaceRange("<h6></h6>",{line: cursorPos.line, ch: cursorPos.ch});
+            let cursorPos = editors[currentEditor].getCursor();
+            editors[currentEditor].replaceRange("<h6></h6>",{line: cursorPos.line, ch: cursorPos.ch});
             toggleDisplay('dropdown__menu--tags');
         });
 }
 
+function pagePreview() {
+    console.log(currentProject);
+    let path = savesPath.replace(/\\/g, "/");
+    let path2 = path.replace(/\s+/g, '%20');
+    console.log(currentPage);
+    let filePath = editorMode === 'css' ? '/index.html' : '/'+currentPage+'.'+editorMode;
+    console.log(path2+currentProject+filePath)
+    shell.openExternal('file:///'+path2+currentProject+filePath);
+    toggleDisplay('dropdown__menu--preview');
+}
 
-function setTab(tabEls, currentTabId, pageId) {
+// TABS AND PAGES - INITIALISATION
+
+function initCMcontainers(details) {
+    //div container with id, class of 'editor' and style of z-index: 0;
+    //make html containers
+    let pages = details.pages;
+    let container = document.getElementById('pages-container');
+
+    for (let i = 0; i < pages.length; i++) {
+        //div with page id+container
+        let pageCont = document.createElement('div');
+        pageCont.setAttribute('id', 'page-container-'+pages[i]);
+        pageCont.setAttribute('style','z-index:0');       
+        pageCont.setAttribute('class','editor');       
+        //textarea with page id and style of: style="visibility: hidden; min-height: 1px;"
+        let textArea = document.createElement('textarea');
+        textArea.setAttribute('id','editor-'+pages[i]);   
+        textArea.setAttribute('style','visibility: hidden; min-height: 1px;');
+        
+        pageCont.appendChild(textArea);
+        container.appendChild(pageCont);   
+    }
+
+    // then make css page container
+    let cssPageCont = document.createElement('div');
+    cssPageCont.setAttribute('id', 'container-css');
+    cssPageCont.setAttribute('style','z-index:0');       
+    cssPageCont.setAttribute('class','editor');  
+    let cssTextArea = document.createElement('textarea');
+    cssTextArea.setAttribute('id','editor-css');   
+    cssTextArea.setAttribute('style','visibility: hidden; min-height: 1px;');
+
+    cssPageCont.appendChild(cssTextArea);
+    container.appendChild(cssPageCont);   
+}
+
+function initCMInstances(details) {
+    let pages = details.pages;
+    // HTML page settings:
+    let htmlSpec = {
+        lineNumbers: true,
+        mode: 'xml',
+        htmlMode: true,
+        theme: 'lucario',
+        lineWrapping: true,
+        autoCloseTags: true,
+        matchTags: true,
+    }
+    //instance for each page
+    for (let i = 0; i < pages.length; i++) {
+        let currentPageCont = document.getElementById('editor-'+pages[i]);
+        let editor = CodeMirror.fromTextArea(currentPageCont, htmlSpec);
+        editor.setSize("100%", "calc(100vh - 70px)");
+        editors[pages[i]] = editor;
+    }
+
+    //then instance for css file
+    //CSS page settings:
+    let cssSpec = {
+        lineNumbers: true,
+        mode: 'css',
+        theme: 'lucario',
+        lineWrapping: true,
+        autoCloseTags: true,
+        matchTags: true,
+    }
+
+    let cssEditorCont = document.getElementById('editor-css');
+    let cssEditor = CodeMirror.fromTextArea(cssEditorCont, cssSpec);
+    cssEditor.setSize("100%", "calc(100vh - 70px)");
+    editors[details.mode+'-'+details.layout] = cssEditor;
+}
+
+function loadPageContent(details) {
+    let pages = details.pages;
+    for (let i = 0; i < pages.length; i++) {
+        fs.readFile(savesPath+details.name+'/'+pages[i]+'.html','utf-8', (err, fileData) => {
+            if (err) return console.log(err);
+  /*           console.log(editors[pages[i]]); */
+            editors[pages[i]].setValue(fileData)
+            editors[pages[i]].clearHistory();
+        });
+    }
+    fs.readFile(savesPath+details.name+'/css/text-default.css','utf-8', (err, fileData) => {
+        if (err) return console.log(err);
+        /* console.log(fileData) */
+        editors["text-default"].setValue(fileData)
+        editors["text-default"].clearHistory();
+    });
+}
+
+function setTab(tabEls, currentTabId, pageId, curPage) {
     let tabs = document.querySelectorAll(tabEls);
     let curTab = document.querySelectorAll(currentTabId)[0];
-    console.log(curTab)
     let page = document.querySelectorAll(pageId)[0];
 
     if (!curTab.classList.contains('active')) {
@@ -238,35 +336,71 @@ function setTab(tabEls, currentTabId, pageId) {
             }
         }
     }
+    currentPage = curPage;
+    currentEditor = curPage;
+    currentPage === 'text-default' ? editorMode = 'css' : editorMode = 'html';
     hideOthers('.editor', page);
 }
 
-function initTabs() {
-    
-    // Index tab
-    let indexTab = document.getElementById('indextab-btn');
-    setTimeout(() => {
-        setTab('.tab-btn', '#indextab-btn', '#editor');
-    },50);
-    indexTab.addEventListener('click', (e) => {
-        setTab('.tab-btn', '#indextab-btn', '#editor');
+function initTabs(details) {
+    //Dynamically create tabs based of pages
+    let tabsList = document.getElementsByClassName('tabs')[0];
+    let pages = details.pages;
+    for (let i = 0; i < pages.length; i++) {
+        let li = document.createElement('li');
+        li.setAttribute('class', 'tab__item');
+        let button = document.createElement('button');
+        button.setAttribute('id',pages[i]+'-tab-btn');
+        button.setAttribute('class','tab-btn');
+        let text = document.createTextNode(pages[i]+'.html');
+
+        button.appendChild(text);
+        li.appendChild(button);
+        tabsList.appendChild(li);
+    }
+
+    // Make CSS tab
+    let cssLi = document.createElement('li');
+    cssLi.setAttribute('class', 'tab__item');
+    let cssButton = document.createElement('button');
+    cssButton.setAttribute('id','css-tab-btn');
+    cssButton.setAttribute('class','tab-btn');
+    let cssText = document.createTextNode('text-default.css');
+
+    cssButton.appendChild(cssText);
+    cssLi.appendChild(cssButton);
+    tabsList.appendChild(cssLi);
+
+    initTabListeners(details);
+}
+
+function initTabListeners(details) {
+
+    let pages = details.pages;
+   // Add listener to all html tabs
+    setTimeout(()=>{
+        setTab('.tab-btn','#'+pages[0]+'-tab-btn', '#page-container-'+pages[0], pages[0]);
+    },100);
+    for (let i = 0; i < pages.length; i++) {
+        let tab = document.getElementById(pages[i]+'-tab-btn');
+        tab.addEventListener('click', (e) => {
+            setTab('.tab-btn','#'+pages[i]+'-tab-btn', '#page-container-'+pages[i], pages[i]);
+        });
+    }
+    // Add listener to css tab
+    let cssTab = document.getElementById('css-tab-btn');
+    cssTab.addEventListener('click', (e) => {
+        setTab('.tab-btn','#css-tab-btn', '#container-css', 'text-default');
     });
 
-    // CSS tab
-    let cssTab = document.getElementById('csstab-btn');
-    cssTab.addEventListener('click', () => {
-        setTab('.tab-btn', '#csstab-btn', '#csseditor')
-    });
-
-    
 }
 
 function saveChanges(currentProject) {
     console.log('saving')
-    let changes = editor.getValue()
+    let changes = editors[currentEditor].getValue();
     let data = {
         name: currentProject,
-        file: 'index.html',//this is hardcoded for now (will be context based later)
+        file: currentPage+'.'+editorMode,//this is hardcoded for now (will be context based later)
         content: changes
     }
     ipcRenderer.send('save:file', data)
